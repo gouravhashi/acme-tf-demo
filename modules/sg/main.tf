ad919d210c05925318268174f5eafc73ae06c1ea
gouravhashi <gourav.sharma@hashicorp.com>
2025-05-22 20:47:31 +0530

    Update main.tf

--- modules/sg/main.tf
+++ modules/sg/main.tf
@@ -1,3 +1,17 @@
 module "sg" {
   source = "git::https://github.com/gouravhashi/acme-tf-demo.git//modules/sg?ref=main"
 }
+module "web_sg" {
+  source               = "gouravhashi/sg/aws"
+  version              = "1.0.0"
+  name                 = "web-sg"
+  description          = "Security group for web servers"
+  vpc_id               = "vpc-123456"
+  ingress_from_port    = 80
+  ingress_to_port      = 80
+  ingress_protocol     = "tcp"
+  ingress_cidr_blocks  = ["0.0.0.0/0"]
+  tags = {
+    Name = "web-sg"
+  }
+}
